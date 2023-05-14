6201ec5c95d481438c5add180e667f043a62e4c9
Markus Petters <mdpetter@ncsu.edu>
2023-05-14 14:15:10 -0400

    simple plotly

---
+++ rxfo/app.js
@@ -0,0 +1,37 @@
+// const {map} = require('ramda')
+
+
+
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("X");
+// output.innerHTML = slider.value;
+// function callback() {
+//     output.innerHTML = add(this.value, 1.5);
+// }
+// slider.oninput = callback;
+
+
+var trace1 = {
+    x: [1, 2, 3, 4],
+    y: [10, 15, 13, 17],
+    mode: 'markers',
+    type: 'scatter'
+  };
+  
+  var trace2 = {
+    x: [2, 3, 4, 5],
+    y: [16, 5, 11, 9],
+    mode: 'lines',
+    type: 'scatter'
+  };
+  
+  var trace3 = {
+    x: [1, 2, 3, 4],
+    y: [12, 9, 15, 12],
+    mode: 'lines+markers',
+    type: 'scatter'
+  };
+  
+  var data = [trace1, trace2, trace3];
+  
+  Plotly.newPlot('myDiv', data);
