4307c191bb55b4bf5ba864ea5e172b09e2936307
Markus Petters <mdpetter@ncsu.edu>
2023-05-14 20:39:48 -0400

    change to lognormal app

--- rxfo/module.js
+++ rxfo/module.js
@@ -1,7 +1,28 @@
-function add(x, y) {
-    return Number(x) + Number(y)
-}
-
 function lin(n) {
-    return Array(n).fill(0).map((x,i) => i)
+    return Array(n).fill(0).map(function (x, i) { return i; });
+}
+function logspace(x, y, len) {
+    var arr, end, tmp, d;
+    var a = Math.log10(x);
+    var b = Math.log10(y);
+    if (len <= 0) {
+        return [];
+    }
+    end = len - 1;
+    d = (b - a) / end;
+    arr = new Array(len);
+    tmp = a;
+    arr[0] = Math.pow(10, tmp);
+    for (var i = 1; i < end; i++) {
+        tmp += d;
+        arr[i] = Math.pow(10, tmp);
+    }
+    arr[end] = Math.pow(10, b);
+    return arr;
+}
+function lnmode(A, x) {
+    var prefactor = A[0] / (Math.sqrt(2 * Math.PI) * Math.log(A[2]));
+    var div = 2 * Math.pow(Math.log(A[2]), 2);
+    var psd = x.map(function (x) { return prefactor * Math.exp(-Math.pow(Math.log(x / A[1]), 2) / div); });
+    return psd;
 }
