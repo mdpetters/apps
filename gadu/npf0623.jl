54c3a7638fc6e88764d0a8cc775b9efb52f1a8de
Sabin Kasparoglu <skaspar@ncsu.edu>
2023-05-22 11:52:46 -0500

    improved growth rate figure

--- gadu/npf0623.jl
+++ gadu/npf0623.jl
@@ -97,7 +97,7 @@ smps = load_aossmps(ss, ts)
 cpcu = load_cpcu(ss, ts)
 
 t = Dates.format.(smps.t .- Hour(5), "yyyy-mm-dd HH:MM:SS") 
-ii = (smps.Dp .> 8) .& (smps.Dp .< 110)
+ii = (smps.Dp .> 1) .& (smps.Dp .< 110)
 
 myS = smps.S[ii, :]'
 myS[myS.<0] .= NaN
@@ -120,10 +120,10 @@ modek40[modek40 .< 0] .= NaN
 modek50[modek50 .< 0] .= NaN
 
 
-grt = DateTime(2022,6,23,12,18,0):Minute(1):DateTime(2022,6,23,13,40,0) |> collect
+grt = DateTime(2022,6,23,11,50,0):Minute(1):DateTime(2022,6,23,16,00,0) |> collect
 len = Dates.value(grt[end] .- grt[1])  / 1000 
-gr = 15.50./60
-grD = 12.0 .+ gr*(0:1:len) |> collect
+gr = 12.50./45
+grD = 3.0 .+ gr*(0:1:len) |> collect
 
 grt2 = DateTime(2022,6,23,09,48,0):Minute(1):DateTime(2022,6,23,13,10,0) |> collect
 len2 = Dates.value(grt2[end] .- grt2[1])  / 1000 
